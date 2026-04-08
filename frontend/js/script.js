e88ed5b39688f5a6c326a864631bbe471abd9122
Juan Manuel Restrepo <Luiyer Rios>
2026-04-08 01:44:13 -0500

    issue HU-002 Verify credentials: Creo el bloque de codigo, que simula la vverificacion del email sin usar base de datos a travs de un patron y devuelvar el resultado

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -14,10 +14,18 @@ form.addEventListener('submit', function(event){
         alert('Password is required');
         return
     }
-    if(!validateEmail(email.value)){
+    if(!validateEmail(email.value)){/*Esta funcion valida el formato del email */
         alert('Please enter a valid email address');
         return;
     }
     alert('Login successful');
     form.reset();/*Esto limpia el formulario despues de enviar */
 });
+/*Esta funcion valida el formato del email usando una expresion regular */
+function validateEmail(emailValue){
+
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;/*Esta expresion regular verifica que el email tenga un formato correcto, con caracteres antes y despues del @ y un dominio valido */
+
+    return regex.test(emailValue);
+
+}
